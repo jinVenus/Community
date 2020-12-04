67e1af7e6435915d39e2d0d963eaa04b9b83860e
xiaojin <xiaojin7656@gmail.com>
2020-12-04 17:48:45 +0100

    登录界面 生成验证码 kaptcha cookies生成

---
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -0,0 +1,102 @@
+package com.nowcoder.community.controller;
+
+
+import com.google.code.kaptcha.Producer;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.OutputStream;
+import java.util.Map;
+
+
+@Controller
+public class LoginController implements CommunityConstant {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private Producer kaptcha;
+
+    @RequestMapping(path = "/register", method = RequestMethod.GET)
+    public String getRegisterPage() {
+        return "/site/register";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
+        return "/site/login";
+    }
+
+    @RequestMapping(path = "/register", method = RequestMethod.POST)
+    public String register(Model model, User user) {
+        Map<String, Object> map = userService.register(user);
+        if (map == null || map.isEmpty()) {
+            model.addAttribute("msg", "注册成功， 我们已经向您的邮箱发送了一封激活邮件，请尽快激活");
+            model.addAttribute("target", "/index");
+            return "/site/operate-result";
+        } else {
+            model.addAttribute("usernameMsg", map.get("usernameMsg"));
+            model.addAttribute("passwordMsg", map.get("passwordMsg"));
+            model.addAttribute("emailMsg", map.get("emailMsg"));
+            return "/site/register";
+        }
+    }
+
+    @RequestMapping(path = "/activation/{userId}/{code}", method = RequestMethod.GET)
+    public String activation(Model model, @PathVariable("userId") int userId, @PathVariable("code") String code) {
+
+        int result = userService.activation(userId, code);
+        if (result == ACTIVATIB_SUCCESS) {
+            model.addAttribute("msg", "激活成功");
+            model.addAttribute("target", "/login");
+        } else if (result == ACTIVATIB_REPEAT) {
+            model.addAttribute("msg", "已经激活过");
+            model.addAttribute("target", "/index");
+
+        } else {
+            model.addAttribute("msg", "激活失败，激活码错误");
+            model.addAttribute("target", "/index");
+        }
+        return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        //生成验证码
+        String text = kaptcha.createText();
+        BufferedImage image = kaptcha.createImage(text);
+
+        //将验证码存入session
+        session.setAttribute("kaptcha", text);
+
+        //将图片输出给浏览器
+
+        response.setContentType("image/png");
+        try {
+
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+
+        } catch (Exception e) {
+            logger.error("响应验证码失败" + e.getMessage());
+
+
+        }
+    }
+}
---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,11 @@
+package com.nowcoder.community.util;
+
+public interface CommunityConstant {
+
+    //激活成功
+    int ACTIVATIB_SUCCESS = 0;
+
+    int ACTIVATIB_REPEAT = 0;
+
+    int ACTIVATIE_FAILURE = 0;
+}
