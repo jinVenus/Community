7294772206c55f67b8eba879dbe7792e6f18ccaa
xiaojin <xiaojin7656@gmail.com>
2020-12-09 20:18:22 +0100

    上传头像 更改密码

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -175,4 +175,13 @@ public class UserService implements CommunityConstant {
     public LoginTicket findeLoginTicket(String ticket) {
         return loginTicketMapper.selectByTicket(ticket);
     }
+
+
+    public int updateHeader(int userId, String headerUrl) {
+        return userMapper.updateHeader(userId, headerUrl);
+    }
+
+    public int updatePassword(int userId, String password){
+        return userMapper.updatePassword(userId, password);
+    }
 }
