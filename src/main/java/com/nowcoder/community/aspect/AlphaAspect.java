07cda9732c648c59903360c2248972457053b113
xiaojin <xiaojin7656@gmail.com>
2020-12-26 13:23:13 +0100

    同意记录日志， AOP AspectJ使用

---
+++ src/main/java/com/nowcoder/community/aspect/AlphaAspect.java
@@ -0,0 +1,44 @@
+package com.nowcoder.community.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+//@Component
+//@Aspect
+public class AlphaAspect {
+
+    //对哪个包所有的组件所有的方法所有的参数
+    @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
+    public void pointcut(){
+
+    }
+
+    @Before("pointcut()")
+    public void before(){
+        System.out.println("before");
+    }
+
+    @After("pointcut()")
+    public void after(){
+        System.out.println("after");
+    }
+
+    @AfterReturning("pointcut()")
+    public void afterReturning(){
+        System.out.println("afterReturning");
+    }
+
+    @AfterThrowing("pointcut()")
+    public void afterThrowing(){
+        System.out.println("afterThrowing");
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable{
+        System.out.println("around");
+        Object obj = joinPoint.proceed();
+        System.out.println("around after");
+        return obj;
+    }
+}
