b809ea823bbd4757684c67581621c3e2245d6741
xiaojin <xiaojin7656@gmail.com>
2020-12-29 18:13:31 +0100

    关注

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -1,11 +1,16 @@
 package com.nowcoder.community.util;
 
 
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
 public class RedisKeyUtil {
 
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
     private static final String PREFIX_USER_LIKE = "like:user";
+    private static final String PREFIX_FOLLOWEE = "followee";
+    private static final String PREFIX_FOLLOWER = "follower";
+
 
     //某个实体的赞
     //like:entity:entityType:entityId -> set(userId)
@@ -16,5 +21,17 @@ public class RedisKeyUtil {
     //某个用户的赞
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+    //某个用户关注的实体
+    //followee:userId:entityType -> zset(entityId, now)
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // 某个用户拥有的粉丝
+    //follower:entityType:entityId -> zset(userId,now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
     }
 }
